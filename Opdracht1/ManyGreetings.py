500d5dd3e565edbea8a86a23597e8a45bc351fc0
PlopKonijn <Plopkonijn@gmail.com>
2015-04-30 01:32:15 +0200

    third commit

---
+++ Opdracht1/ManyGreetings.py
@@ -0,0 +1,3 @@
+name = input("What is you name? ")
+count = input("How many greetings? ")
+print(count + ' x Hello, ' + name + '!')
--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # TEST
+=======
+TEXT
+>>>>>>> 01d8a5c335ba941b58babc6e60faeac79cb5997c
