7b80f4d88aa5df753cfc362057650ab13be30952
PlopKonijn <Plopkonijn@gmail.com>
2015-04-30 01:27:49 +0200

    second commit

---
+++ Opdracht2/Sieve_of_E.py
@@ -0,0 +1,23 @@
+import sys
+import math
+import time
+
+def primes_sieve_E1(limit):
+	Sieve = [True] * (limit//2)
+	Sieve[0] = False
+	yield 2
+	for (i,isprime) in enumerate(Sieve):
+		if isprime:
+			yield (2*i)+1
+			for j in range((2*i+1)**2,limit,2*(2*i+1)):
+				Sieve[j//2] = False
+
+N = int(sys.argv[1])
+T1 = time.time()
+primes = list(primes_sieve_E1(N))
+T2 = time.time()
+print('Found', len(primes),'prime numbers smaller than',N,'in',T2-T1,'sec')						
+pout = open(sys.argv[2],'w')
+for p in primes:
+	pout.write(str(p)+'\n')
+pout.close()
