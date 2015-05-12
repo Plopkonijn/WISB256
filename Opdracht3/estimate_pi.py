32625eb3061b38982c9d6e698673b4fdcce6ef71
PlopKonijn <PlopKonijn@gmail.com>
2015-05-12 16:29:39 +0200

    finished estimating pi

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -1,25 +1,25 @@
-import math
-import random
+from math import pi, cos, sqrt, asin
+from random import uniform, seed
+import sys
 
 def drop_needle(L):
-	x0 = random.random()
-	y0 = random.random()
-	phi = random.vonmisesvariate(0,0)
-	x1 = x0 + L * math.cos(phi)
-	y1 = y0 + L * math.sin(phi)
-	if int(y0) !=int(y1):
+	x = uniform(0,.5)
+	phi = uniform(0,pi/2)
+	xtip = x - L*cos(phi)/2
+	if xtip <0: 
 		return True
-	else:
-		return False
+	return False
+		
+N = int(sys.argv[1])
+L = float(sys.argv[2])
+if len(sys.argv)>3:	seed(int(sys.argv[3]))
+C = 0
+
+for i in range(N):
+	if drop_needle(L): C+=1
 	
-	
-	
-	
-	
-count = 1000000
-pi = 0
-for i in range(count):
-	if(drop_needle(1)):
-		pi = pi +1
-print(count/pi)
-print(math.pi)
+print(C,'hits in',N,'tries')
+C = C/N
+PI = 2*L/C	
+if(L>1):PI = 2*(L - sqrt(L*L-1) - asin(1/L))/(C-1)
+print('Pi =',PI)
