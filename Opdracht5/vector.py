ad4a8ca1582cbfa5e201962b4bbcf8686076ef75
PlopKonijn <PlopKonijn@gmail.com>
2015-05-21 14:56:26 +0200

    debuggin deleted

--- Opdracht5/vector.py
+++ Opdracht5/vector.py
@@ -47,18 +47,6 @@ def GrammSchmidt(vectors):
 	
 	return result
 
-v0 = Vector(2,[3,1])
-v1 = Vector(2,[2,2])
-W = GrammSchmidt([v0,v1])
-print(W[0])
-
-print(W[1])
-			
-print(W[0].inner(W[1]))
-
-print(W[0].norm())
-
-print(W[1].norm())
 		
 	
 	
